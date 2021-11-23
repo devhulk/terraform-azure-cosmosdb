5869c47d8820724d4a0c108f38276ef55c440189
devhulk <gry2big93@gmail.com>
2021-11-23 17:57:15 -0500

    rm data ref

--- main.tf
+++ main.tf
@@ -42,15 +42,15 @@ resource "azurerm_cosmosdb_account" "example" {
 resource "azurerm_cosmosdb_sql_database" "example" {
   count =               var.db == "sql" ? 1 : 0 
   name                = "${var.team_name}-cosmos-mongo-db"
-  resource_group_name = data.azurerm_cosmosdb_account.example.resource_group_name
-  account_name        = data.azurerm_cosmosdb_account.example.name
+  resource_group_name = azurerm_cosmosdb_account.example.resource_group_name
+  account_name        = azurerm_cosmosdb_account.example.name
   throughput          = 400
 }
 
 resource "azurerm_cosmosdb_mongo_database" "example" {
   count =               var.db == "mongo" ? 1 : 0 
   name                = "${var.team_name}-cosmos-mongo-db"
-  resource_group_name = data.azurerm_cosmosdb_account.example.resource_group_name
-  account_name        = data.azurerm_cosmosdb_account.example.name
+  resource_group_name = azurerm_cosmosdb_account.example.resource_group_name
+  account_name        = azurerm_cosmosdb_account.example.name
   throughput          = 400
 }
