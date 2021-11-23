6d1ddd05cee3876efb880ac9aa55f6a9b5cbd7fa
devhulk <gry2big93@gmail.com>
2021-11-23 18:53:56 -0500

    sensitive output false

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "read_endpoints" {
     value = azurerm_cosmosdb_account.example.read_endpoints
-    sensitive = true
+    sensitive = false
 }
 
 output "write_endpoints" {
     value = azurerm_cosmosdb_account.example.write_endpoints
-    sensitive = true
+    sensitive = false
 }
 
 output "connection_strings" {
     value = azurerm_cosmosdb_account.example.connection_strings
-    sensitive = true
+    sensitive = false
 }
 
